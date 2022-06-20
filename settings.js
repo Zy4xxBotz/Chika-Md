15ce39ac98af9c6f9c28d7e706e904e795b8f133
Zy4xxBotz <107827609+Zy4xxBotz@users.noreply.github.com>
2022-06-20 11:32:25 +0700

    Create settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @AmiBotz
+› Base Ori @Zy4xxBotz
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['083160276131','083160276131','083160276131'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['085161973159'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '083160276131' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Ami' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'AmiBotz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Zy4xxBotz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'Ami YT'
+global.ig = 'Zy4xxBotz''
+global.mygc = 'https://chat.whatsapp.com/083160276131'
+global.myweb = 'https://s.id/Zy4xxBotz'
+global.email = 'azfaming1123@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© AmiBotz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Zy4xxBotz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 10,
 }
 
 //—————「 Set Image 」—————//
